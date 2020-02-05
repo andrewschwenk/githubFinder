22d73739157d00db0e860c93837bf163662c4b43
Andrew Schwenk <andrew.schwenk@gmail.com>
2020-02-05 16:50:27 -0500

    link to user github profile target set to blank

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -52,7 +52,7 @@ const User = ({ match }) => {
         <h3>Bio</h3>
         <p>{bio}</p>
       </Fragment>)}
-      <a href={html_url} className="btn btn-dark my-1">Visit GitHub Profile</a>
+      <a href={html_url} target="_blank" rel="noopener noreferrer" className="btn btn-dark my-1">Visit GitHub Profile</a>
       <ul>
         <li>
           {login && <Fragment>
